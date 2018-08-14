8396e1f89ead6a1022a99904c24a0575ad56f530
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2018-08-14 19:13:55 +1000

    Just print things

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-export default ({ types: t }) => ({
+import { CLIEngine } from 'eslint';
+
+export default () => ({
+  pre(state) {
+    const cli = new CLIEngine();
+    console.log(cli.executeOnText(state.code));
+  },
+  post() {
+  },
 });
