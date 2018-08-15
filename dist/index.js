cd6383fd5b603d0af9a5ba4c958ddf6d37219aa5
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2018-08-15 12:19:20 +1000

    Pass opts.eslintOpts through to eslint

--- dist/index.js
+++ dist/index.js
@@ -15,10 +15,16 @@ function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = 
 
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 var _default = function _default() {
   return {
     pre: function pre(state) {
-      var cli = new _eslint.CLIEngine();
+      var opts = _objectSpread({}, state.opts);
+
+      var cli = new _eslint.CLIEngine(opts.eslintOpts);
 
       var _cli$executeOnText = cli.executeOnText(state.code),
           results = _cli$executeOnText.results;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import { CLIEngine } from 'eslint';
 
 export default () => ({
   pre(state) {
-    const cli = new CLIEngine();
+    const opts = {
+      ...state.opts,
+    };
+    const cli = new CLIEngine(opts.eslintOpts);
     const { results } = cli.executeOnText(state.code)
     const { results: [result] } = cli.executeOnText(state.code);
     console.log(result);
